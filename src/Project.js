7dc6f6b3d67e1c539f58081adbe5f884a39c9fce
Chris Griffiths <chrisgriffiths635@outlook.com>
2021-07-05 20:45:04 +0900

    changed files

--- src/Project.js
+++ src/Project.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Thumbnail from './thumbnail.js';
-import ToDoImage from './to-do-list.png';
+import ToDoImage from './images/to-do-list.png';
 import './App.css';
 
 function Project(props) {
     return (
         <div>
-            <h1 class="title">Project</h1>
+            <h1 class="title">Projects</h1>
             <Thumbnail
               link="https://cgriffiths5.github.io/to-do-list/"
               image={ToDoImage}
