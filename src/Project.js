35dae9292436cb726136a726702d419f72c34a26
Chris Griffiths <chrisgriffiths635@outlook.com>
2021-07-06 19:48:17 +0900

    added weather app

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Thumbnail from './thumbnail.js';
 import ToDoImage from './images/to-do-list.png';
+import WeatherAppImage from './images/weather-app.png'
 import './App.css';
 
 function Project(props) {
@@ -11,6 +12,12 @@ function Project(props) {
               link="https://cgriffiths5.github.io/to-do-list/"
               image={ToDoImage}
               title="To Do List"
+              category="Desktop App"
+            />
+            <Thumbnail
+              link="https://cgriffiths5.github.io/weather_app/"
+              image={WeatherAppImage}
+              title="Weather App"
               category="Desktop App"
             />
         </div>
