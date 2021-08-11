a3d56bcc114bfa188b46567de5d1678e4bbcb653
Chris Griffiths <chrisgriffiths635@outlook.com>
2021-08-11 11:23:10 +0100

    README update

--- src/About.js
+++ src/About.js
@@ -8,6 +8,12 @@ function About(props) {
                 My name is Chris. I am currently learning HTML, CSS, and Javascript. This is a porfolio site that 
                 will contain some of my projects. Most of the projects use the React framework.
             </p>
+            <h1 class="title">Skills</h1>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript (React)</li>
+                </ul>
         </div>
     )
 }
